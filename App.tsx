89afe6d59efeb3b9c6f986615682f6de1709baed
Martin Jobbagy <mato.jobbagy@gmail.com>
2020-04-14 08:13:59 +0200

    initial commit

---
+++ App.tsx
@@ -0,0 +1,81 @@
+import * as React from "react";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
+import { SplashScreen } from "expo";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import store from "./src/redux/store";
+import TabNavigator from "./src/navigation/TabNavigator";
+import useLinking from "./src/navigation/useLinking";
+
+const Stack = createStackNavigator();
+const headerMode = Platform.OS === "web" ? "none" : "screen";
+
+interface IApp {
+    skipLoadingScreen: any;
+}
+
+export default function App(props: IApp) {
+    const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+    const [initialNavigationState, setInitialNavigationState] = React.useState();
+    const containerRef = React.useRef();
+    const { getInitialState } = useLinking(containerRef);
+
+    // Load any resources or data that we need prior to rendering the app
+    React.useEffect(() => {
+        async function loadResourcesAndDataAsync() {
+            try {
+                SplashScreen.preventAutoHide();
+
+                // Load our initial navigation state
+                setInitialNavigationState(await getInitialState());
+
+                // Load fonts
+                await Font.loadAsync({
+                    ...Ionicons.font,
+                    "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf"),
+                });
+            } catch (e) {
+                // We might want to provide this error information to an error reporting service
+                console.warn(e);
+            } finally {
+                setLoadingComplete(true);
+                SplashScreen.hide();
+            }
+        }
+
+        loadResourcesAndDataAsync();
+    }, []);
+
+    if (!isLoadingComplete && !props.skipLoadingScreen) {
+        return null;
+    } else {
+        return (
+            <Provider store={store.reduxStore}>
+                <PersistGate loading={null} persistor={store.persistor}>
+                    <View style={styles.container}>
+                        {Platform.OS === "ios" && <StatusBar barStyle="default" />}
+                        <NavigationContainer
+                            ref={containerRef}
+                            initialState={initialNavigationState}
+                        >
+                            <Stack.Navigator headerMode={headerMode}>
+                                <Stack.Screen name="Root" component={TabNavigator} />
+                            </Stack.Navigator>
+                        </NavigationContainer>
+                    </View>
+                </PersistGate>
+            </Provider>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+});
---
+++ src/navigation/TabNavigator.js
@@ -0,0 +1,91 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
+import TabBarIcon from "../components/TabBarIcon";
+import HomeScreen from "../screens/HomeScreen";
+import LinksScreen from "../screens/LinksScreen";
+import { Platform } from "react-native";
+import device from "../../constants/Layout";
+
+const { window, isBigDevice } = device;
+const Tab = Platform.OS === "web" ? createMaterialTopTabNavigator() : createBottomTabNavigator();
+const INITIAL_ROUTE_NAME = "Home";
+
+const tabBarOptions = {
+    showIcon: true,
+};
+
+const HomeStack = createStackNavigator();
+
+function HomeStackScreen() {
+    return (
+        <HomeStack.Navigator
+            // screenOptions={{
+            //     cardStyle: {
+            //         width: isBigDevice ? 1000 : window.width,
+            //         alignSelf: "center"
+            //     }
+            // }}
+            headerMode={"none"}
+        >
+            <HomeStack.Screen name="Home" component={HomeScreen} />
+            <HomeStack.Screen name="HomeLinks" component={LinksScreen} />
+        </HomeStack.Navigator>
+    );
+}
+
+export default function TabNavigator({ navigation, route }) {
+    // Set the header title on the parent stack navigator depending on the
+    // currently active tab. Learn more in the documentation:
+    // https://reactnavigation.org/docs/en/screen-options-resolution.html
+    navigation.setOptions({ headerTitle: getHeaderTitle(route) });
+
+    return (
+        <Tab.Navigator
+            style={{ paddingHorizontal: 0 }}
+            tabBarOptions={tabBarOptions}
+            initialRouteName={INITIAL_ROUTE_NAME}
+        >
+            <Tab.Screen
+                name="Home"
+                component={HomeStackScreen}
+                options={{
+                    title: "Get Started",
+                    tabBarIcon: ({ focused }) => (
+                        <TabBarIcon focused={focused} name="md-code-working" />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Links"
+                component={LinksScreen}
+                options={{
+                    title: "Resources",
+                    tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
+                }}
+            />
+            <Tab.Screen
+                name="Settings"
+                component={LinksScreen}
+                options={{
+                    title: "Settings",
+                    tabBarIcon: ({ focused }) => (
+                        <TabBarIcon focused={focused} name="md-settings" />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
+
+function getHeaderTitle(route) {
+    const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
+
+    switch (routeName) {
+        case "Home":
+            return "How to get started";
+        case "Links":
+            return "Links to learn more";
+    }
+}
